193dec0f5a44b002d41dfb59c13249d6b8d79c36
Rehman Ali <rehmanali001@gmail.com>
2019-08-01 00:23:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,10 +14,10 @@ class Dog
   def self.clear_all
     @@all.clear
   end
-  def self.print_all
-    @@all.each { |dog| puts dog } 
-  end
   def save
     @@all << self
+  end
+    def self.print_all
+    @@all.each { |dog| puts dog } 
   end
 end 
